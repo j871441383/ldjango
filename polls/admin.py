9add73c68df586933c962e03499db8734c5df99c
荆 <871441383@qq.com>
2018-04-13 18:08:25 +0800

    django中文文档第二部分

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from polls.models import Question
+from polls.models import Choice
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,   {"fields":["question_text"]}),
+        ("详情",   {"fields":["question_text","pub_date"],'classes': ['collapse']}),
+    ]
+
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
